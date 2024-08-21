8412bd66139f8799defcf6a978bd586d41388adb
Gregory Moyer <greg@commitpush.run>
2024-08-21 03:25:22 +0000

    Implement Distance

--- src/space.rs
+++ src/space.rs
@@ -13,6 +13,86 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/// Defines the absolute separation between two [`Location`] instances.
+#[derive(PartialEq, Eq, PartialOrd, Ord, Debug)]
+pub struct Distance {
+    x: i32,
+    y: i32,
+    z: i32,
+}
+
+static ZERO: Distance = Distance { x: 0, y: 0, z: 0 };
+static MAX: Distance = Distance {
+    x: i32::MAX,
+    y: i32::MAX,
+    z: i32::MAX,
+};
+
+pub trait OfDistance {
+    fn of(self) -> Distance;
+}
+
+impl Distance {
+    pub fn zero() -> &'static Distance {
+        return &ZERO;
+    }
+
+    pub fn max() -> &'static Distance {
+        return &MAX;
+    }
+
+    pub fn of<A>(args: A) -> Distance
+    where
+        A: OfDistance,
+    {
+        args.of()
+    }
+
+    pub fn x(&self) -> i32 {
+        return self.x;
+    }
+
+    pub fn y(&self) -> i32 {
+        return self.y;
+    }
+
+    pub fn z(&self) -> i32 {
+        return self.z;
+    }
+
+    pub fn between(start: &Location, end: &Location) -> Distance {
+        return Distance::of((
+            end.x() - start.x(),
+            end.y() - start.y(),
+            end.z() - start.z(),
+        ));
+    }
+
+    pub fn is_within(&self, distance: Distance) -> bool {
+        return self.x <= distance.x() && self.y <= distance.y() && self.z <= distance.z();
+    }
+}
+
+impl OfDistance for (i32, i32) {
+    fn of(self) -> Distance {
+        return Distance {
+            x: self.0.abs(),
+            y: self.1.abs(),
+            z: 0,
+        };
+    }
+}
+
+impl OfDistance for (i32, i32, i32) {
+    fn of(self) -> Distance {
+        return Distance {
+            x: self.0.abs(),
+            y: self.1.abs(),
+            z: self.2.abs(),
+        };
+    }
+}
+
 /// Defines a location in space without concern for what may or may not be at that location.
 #[derive(PartialEq, Eq, PartialOrd, Ord, Debug)]
 pub struct Location {
@@ -21,14 +101,14 @@ pub struct Location {
     z: i32,
 }
 
-pub trait At {
+pub trait AtLocation {
     fn at(self) -> Location;
 }
 
 impl Location {
     pub fn at<A>(args: A) -> Location
     where
-        A: At,
+        A: AtLocation,
     {
         args.at()
     }
@@ -46,11 +126,15 @@ impl Location {
     }
 
     pub fn go(&self, vector: &Vector) -> Location {
-        return Location::at((self.x + vector.x(), self.y + vector.y(), self.z + vector.z()))
+        return Location::at((
+            self.x + vector.x(),
+            self.y + vector.y(),
+            self.z + vector.z(),
+        ));
     }
 }
 
-impl At for (i32, i32) {
+impl AtLocation for (i32, i32) {
     fn at(self) -> Location {
         return Location {
             x: self.0,
@@ -60,7 +144,7 @@ impl At for (i32, i32) {
     }
 }
 
-impl At for (i32, i32, i32) {
+impl AtLocation for (i32, i32, i32) {
     fn at(self) -> Location {
         return Location {
             x: self.0,
@@ -70,7 +154,7 @@ impl At for (i32, i32, i32) {
     }
 }
 
-/// Defines the separation between two [`Location`] objects.
+/// Defines the distance and direction to go from one [`Location`] to another.
 #[derive(PartialEq, Eq, PartialOrd, Ord, Debug)]
 pub struct Vector {
     x: i32,
@@ -78,14 +162,14 @@ pub struct Vector {
     z: i32,
 }
 
-pub trait Of {
+pub trait OfVector {
     fn of(self) -> Vector;
 }
 
 impl Vector {
     pub fn of<A>(args: A) -> Vector
     where
-        A: Of,
+        A: OfVector,
     {
         args.of()
     }
@@ -103,11 +187,15 @@ impl Vector {
     }
 
     pub fn from(start: &Location, end: &Location) -> Vector {
-        return Vector::of((end.x() - start.x(), end.y() - start.y(), end.z() - start.z()));
+        return Vector::of((
+            end.x() - start.x(),
+            end.y() - start.y(),
+            end.z() - start.z(),
+        ));
     }
 }
 
-impl Of for (i32, i32) {
+impl OfVector for (i32, i32) {
     fn of(self) -> Vector {
         return Vector {
             x: self.0,
@@ -117,7 +205,7 @@ impl Of for (i32, i32) {
     }
 }
 
-impl Of for (i32, i32, i32) {
+impl OfVector for (i32, i32, i32) {
     fn of(self) -> Vector {
         return Vector {
             x: self.0,
--- src/space_tests.rs
+++ src/space_tests.rs
@@ -1,3 +1,4 @@
+use crate::space::Distance;
 /*
  * Copyright © 2024 Gregory P. Moyer
  *
@@ -16,6 +17,93 @@
 use crate::space::Location;
 use crate::space::Vector;
 use rstest::rstest;
+
+// Distance start =====
+
+#[test]
+fn distance_zero() {
+    // when
+    let result = Distance::zero();
+
+    // then
+    assert_eq!(result.x(), 0);
+    assert_eq!(result.y(), 0);
+    assert_eq!(result.z(), 0);
+}
+
+#[test]
+fn distance_max() {
+    // when
+    let result = Distance::max();
+
+    // then
+    assert_eq!(result.x(), i32::MAX);
+    assert_eq!(result.y(), i32::MAX);
+    assert_eq!(result.z(), i32::MAX);
+}
+
+#[rstest]
+#[case(1, 1, 1, 2, 2, 2, 1, 1, 1)]
+#[case(2, 2, 2, 1, 1, 1, 1, 1, 1)]
+#[case(0, 0, 0, 1, 0, 0, 1, 0, 0)]
+#[case(0, 0, 0, 0, 1, 0, 0, 1, 0)]
+#[case(0, 0, 0, 0, 0, 1, 0, 0, 1)]
+fn distance_between(
+    #[case] start_x: i32,
+    #[case] start_y: i32,
+    #[case] start_z: i32,
+    #[case] end_x: i32,
+    #[case] end_y: i32,
+    #[case] end_z: i32,
+    #[case] distance_x: i32,
+    #[case] distance_y: i32,
+    #[case] distance_z: i32,
+) {
+    // given
+    let start = Location::at((start_x, start_y, start_z));
+    let end = Location::at((end_x, end_y, end_z));
+
+    // when
+    let result = Distance::between(&start, &end);
+
+    // then
+    assert_eq!(result, Distance::of((distance_x, distance_y, distance_z)));
+}
+
+#[test]
+fn distance_of_xy() {
+    // given
+    let x = 1;
+    let y = 2;
+
+    // when
+    let result = Distance::of((x, y));
+
+    // then
+    assert_eq!(result.x(), x);
+    assert_eq!(result.y(), y);
+    assert_eq!(result.z(), 0);
+}
+
+#[test]
+fn distance_of_xyz() {
+    // given
+    let x = 1;
+    let y = 2;
+    let z = 3;
+
+    // when
+    let result = Distance::of((x, y, z));
+
+    // then
+    assert_eq!(result.x(), x);
+    assert_eq!(result.y(), y);
+    assert_eq!(result.z(), z);
+}
+
+// TODO finish tests for Distance
+
+// Distance end =====
 
 // Location start =====
 
